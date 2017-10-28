7d12dee07870866144be45e298dcd706c15c433e
Todsphol <theaddza11@gmail.com>
2017-10-28 18:02:36 +0700

    First commit

---
+++ app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainActivity.kt
@@ -0,0 +1,12 @@
+package add.project.micro.co.th.projectmicroprocessor
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
