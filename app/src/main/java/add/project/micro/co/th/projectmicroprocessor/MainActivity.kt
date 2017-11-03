e303cf1f5e3be8f185e090a61eec6bf2e2780fab
Todsphol <theaddza11@gmail.com>
2017-11-03 18:03:54 +0700

    Create new card view

--- app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainActivity.kt
+++ app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainActivity.kt
@@ -2,11 +2,23 @@ package add.project.micro.co.th.projectmicroprocessor
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.LinearLayout
+
 
 class MainActivity : AppCompatActivity() {
+    private var mRecyclerView : RecyclerView?= null
+    private var mAdapter: RecyclerView.Adapter<*>? = null
+    private var mLayoutManager: RecyclerView.LayoutManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mRecyclerView = findViewById(R.id.my_recycler_view)
+        mRecyclerView?.setHasFixedSize(true)
+        mLayoutManager = LinearLayoutManager(this)
+        mRecyclerView?.layoutManager = mLayoutManager
+
     }
 }
