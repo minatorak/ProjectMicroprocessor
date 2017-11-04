14c898ef49ce48b17eb27acd9066df785508086f
Todsphol <theaddza11@gmail.com>
2017-11-05 01:07:15 +0700

    Using fragment replace activity

--- app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainActivity.kt
+++ app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainActivity.kt
@@ -3,24 +3,25 @@ package add.project.micro.co.th.projectmicroprocessor
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
-
+import butterknife.ButterKnife
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        ButterKnife.bind(this)
+        initFragment()
 
     }
-//    fun initFragment() {
-//        supportFragmentManager.beginTransaction()
-//                .replace(R.id.container, MainFragment.newInstance())
-//                .commit()
-//    }
-//    fun changeFragment(fragment: Fragment) {
-//        supportFragmentManager.beginTransaction()
-//                .replace(R.id.container, fragment)
-//                .addToBackStack(null)
-//                .commit()
-//    }
+    fun initFragment() {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commit()
+    }
+    fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .addToBackStack(null)
+                .commit()
+    }
 }
--- app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainFragment.kt
+++ app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainFragment.kt
@@ -6,12 +6,14 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import butterknife.ButterKnife
 
 class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
+        ButterKnife.bind(this,view)
         return view
     }
 
