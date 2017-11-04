5fc613f44a216922aac415714954cc0ffdfff1b9
Todsphol <theaddza11@gmail.com>
2017-11-05 00:03:51 +0700

    Create new item row

---
+++ app/src/main/java/add/project/micro/co/th/projectmicroprocessor/CustomCardView.java
@@ -0,0 +1,38 @@
+package add.project.micro.co.th.projectmicroprocessor;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+
+/**
+ * Created by add on 4/11/2560.
+ */
+
+public class CustomCardView extends CardView {
+    TypedValue outValue;
+    public CustomCardView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CustomCardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomCardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+    private void init() {
+        outValue = new TypedValue();
+
+        setClickable(true);
+        setRadius(0f);
+        setPreventCornerOverlap(false);
+        getContext().getTheme().resolveAttribute(android.R.attr.selectableItemBackground, outValue, true);
+        setForeground(ContextCompat.getDrawable(getContext(), outValue.resourceId));
+    }
+}
