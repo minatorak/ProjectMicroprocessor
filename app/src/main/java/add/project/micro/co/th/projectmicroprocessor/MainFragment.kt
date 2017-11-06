d8a1da0dd57a57fea8ce99871244b1ad738a734a
Todsphol <theaddza11@gmail.com>
2017-11-06 19:31:18 +0700

    can i get value in database

--- app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainFragment.kt
+++ app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainFragment.kt
@@ -2,6 +2,7 @@ package add.project.micro.co.th.projectmicroprocessor
 
 
 import android.os.Bundle
+import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,25 +12,24 @@ import android.widget.TextView
 import butterknife.BindView
 import butterknife.ButterKnife
 import com.google.firebase.database.FirebaseDatabase
-import java.util.*
-import android.content.ContentValues.TAG
-import android.widget.Toast
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.ValueEventListener
 
 
-
 class MainFragment : Fragment() {
     var baseR = FirebaseDatabase.getInstance().getReference()
     var logR = baseR.child("log")
-    var Valuex = arrayListOf<ModelMapper>()
-    @BindView(R.id.im_washing) lateinit var imageView : ImageView
-    @BindView(R.id.tv_real_time) lateinit var LeftTime : TextView
+    var positionR = baseR.child("position")
+    var status = baseR.child("status")
+    var values: ArrayList<ModelMapper?> = ArrayList()
+    @Nullable @BindView(R.id.im_washing) lateinit var imageView : ImageView
+    @Nullable @BindView(R.id.tv_real_time) lateinit var LeftTime : TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_main, container, false)!!
+        ButterKnife.bind(this, view)
         getSet()
         return view
     }
@@ -37,8 +37,8 @@ class MainFragment : Fragment() {
     private fun getSet() {
         logR.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val value = dataSnapshot.getValue(String::class.java)
-
+                val name  = dataSnapshot.child("count").value
+                LeftTime.text = name.toString()
             }
 
             override fun onCancelled(error: DatabaseError) {
