b14c21d6404a5f566fa3af057c15123d1b0a19ee
Todsphol <theaddza11@gmail.com>
2017-11-06 17:39:17 +0700

    Call database on firebase

--- app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainActivity.kt
+++ app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainActivity.kt
@@ -4,12 +4,16 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import butterknife.ButterKnife
+
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ButterKnife.bind(this)
+
+
         initFragment()
 
     }
--- app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainFragment.kt
+++ app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainFragment.kt
@@ -3,12 +3,23 @@ package add.project.micro.co.th.projectmicroprocessor
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.widget.ImageViewCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import butterknife.BindView
 import butterknife.ButterKnife
+import com.google.firebase.database.FirebaseDatabase
 
 class MainFragment : Fragment() {
+    var mRootRef = FirebaseDatabase.getInstance().reference
+    var mLogRef = mRootRef.child("log")
+    var mPositionRef = mRootRef.child("position")
+    var mStatusRef = mRootRef.child("status")
+    @BindView(R.id.im_washing) lateinit var imageView : ImageView
+    @BindView(R.id.tv_real_time) lateinit var LeftTime : TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
