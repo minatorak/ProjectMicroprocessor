a8eb11a6864890bea1d47dbb93c654b643b9dfd9
Todsphol <theaddza11@gmail.com>
2017-11-07 16:04:36 +0700

    Set geter database pull to project

--- app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainFragment.kt
+++ app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainFragment.kt
@@ -2,13 +2,17 @@ package add.project.micro.co.th.projectmicroprocessor
 
 
 import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.os.Build
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import butterknife.BindView
@@ -17,17 +21,22 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_main.*
 import java.util.*
+import java.util.concurrent.TimeUnit
 
-
+@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+@SuppressLint("NewApi")
 class MainFragment : Fragment() {
     var baseR = FirebaseDatabase.getInstance().getReference()
+    var tempR = baseR.child("Temp")
     var logR = baseR.child("log")
     var positionR = baseR.child("position")
     var statusR = baseR.child("status")
+    var intervalTime =""
     @Nullable @BindView(R.id.image_washing) lateinit var imageView : ImageView
-    @Nullable @BindView(R.id.tv_real_time) lateinit var LeftTime : TextView
-    @Nullable @BindView(R.id.tv_real_status) lateinit var Status : TextView
+    @Nullable @BindView(R.id.tv_real_time) lateinit var leftTime: TextView
+    @Nullable @BindView(R.id.tv_real_status) lateinit var status: TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -36,6 +45,20 @@ class MainFragment : Fragment() {
         dataLog()
         dataStatus()
         return view
+
+    }
+    private fun dataTemp() {
+        tempR.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+            val celsius = dataSnapshot.child("Celcius").getValue(Float::class.java)
+            val huminity = dataSnapshot.child("Huminity").getValue(Float::class.java)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+
+        })
     }
 
     private fun dataLog() {
@@ -44,18 +67,10 @@ class MainFragment : Fragment() {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 val datetime  = dataSnapshot.child("dateStart").getValue(Long::class.java)
                 val calendar = Calendar.getInstance()
-                val calendar2 = Calendar.getInstance()
-                calendar.timeInMillis = datetime!! + 3600000
-                val mHourAfter = calendar2.get(Calendar.HOUR)
-                val mMinuteAfter = calendar2.get(Calendar.MINUTE)
-                val mSecondAfter = calendar2.get(Calendar.SECOND)
-                val mHourBefore = calendar.get(Calendar.HOUR)
-                val mMinuteBefore = calendar.get(Calendar.MINUTE)
-                val mSecondBefore = calendar.get(Calendar.SECOND)
-                val timeMi = calendar2.timeInMillis
-               val xxxx= (calendar.timeInMillis-timeMi)
-                calendar.timeInMillis=xxxx
-                 LeftTime.text = calendar.get(Calendar.MINUTE).toString()
+                calendar.timeInMillis = datetime!!
+                 intervalTime = calendar.get(Calendar.MINUTE).toString()
+                leftTime.text = intervalTime
+
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -64,16 +79,30 @@ class MainFragment : Fragment() {
 
     }
 
-    fun dataStatus() {
+    private fun dataPosition() {
+        positionR.addValueEventListener(object :ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+            val latitude = dataSnapshot.child("latitude").getValue(Double::class.java)
+            val longitude = dataSnapshot.child("lng").getValue(Double::class.java)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    private fun dataStatus() {
         statusR.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val status = dataSnapshot.child("running").value
-                if (status.toString().equals("0") ) {
+                val statusData = dataSnapshot.child("running").value
+                if (statusData.toString().equals("0") ) {
                     imageView.setColorFilter(ContextCompat.getColor(context, R.color.green))
-                    Status.text = "free"
+                    status.text = "free"
                 }else {
                     imageView.setColorFilter(ContextCompat.getColor(context, R.color.red))
-                    Status.text = "running"
+                    status.text = "running"
 
                 }
 
@@ -81,8 +110,8 @@ class MainFragment : Fragment() {
             override fun onCancelled(error: DatabaseError) {
             }
         })
-
         }
+
 
     companion object {
         fun newInstance(): MainFragment {
