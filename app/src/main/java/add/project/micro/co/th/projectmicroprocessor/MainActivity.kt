df58ca7cc704104bcaef0296adf532efe0c5cd38
Todsphol <theaddza11@gmail.com>
2017-11-08 19:36:00 +0700

    Set title in toolbar is complete

--- app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainActivity.kt
+++ app/src/main/java/add/project/micro/co/th/projectmicroprocessor/MainActivity.kt
@@ -2,17 +2,22 @@ package add.project.micro.co.th.projectmicroprocessor
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
+import android.support.v7.widget.Toolbar
+import android.widget.TextView
+import butterknife.BindView
 import butterknife.ButterKnife
 
 
 class MainActivity : AppCompatActivity() {
+    @Nullable @BindView(R.id.my_toolbar) lateinit var toolBar : Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ButterKnife.bind(this)
-
+        setSupportActionBar(toolBar)
         initFragment()
 
     }
